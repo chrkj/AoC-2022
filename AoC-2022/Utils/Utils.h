e85deb585c5fb5495fdc14ab17a9243599422c9a
Christian Kjær <c-kj@mail.dk>
2022-12-08 18:08:01 +0100

    Updated utils

--- AoC-2022/Utils/Utils.h
+++ AoC-2022/Utils/Utils.h
@@ -1,8 +1,10 @@
+using namespace std;
+
 namespace Utils
 {
 	// convert 2D vector of vector of strings to vector of vector of ints
-	template <typename Int = int, template <typename...> typename Cont2 = std::vector, template <typename...> typename Cont1 = std::vector>
-	Cont1<Cont2<Int>> InputToInt2D(const Cont1<Cont2<std::string, std::allocator<std::string>>, std::allocator<std::vector<std::string, std::allocator<std::string>>>>& input) {
+	template <typename Int = int, template <typename...> typename Cont2 = vector, template <typename...> typename Cont1 = vector>
+	Cont1<Cont2<Int>> InputToInt2D(const Cont1<Cont2<string, allocator<string>>, allocator<vector<string, allocator<string>>>>& input) {
 
 		const size_t& size = input.size();
 		Cont1<Cont2<Int>> output(size);
@@ -12,16 +14,16 @@ namespace Utils
 			Int value;
 			output[i].reserve(input[i].size());
 
-			for (const std::string& word : input[i]) {
-				auto result = std::from_chars(word.data(), word.data() + word.size(), value);
+			for (const string& word : input[i]) {
+				auto result = from_chars(word.data(), word.data() + word.size(), value);
 
-				if (result.ec == std::errc::invalid_argument) {
-					std::cerr << "Non-integer value in input_to_int\n";
-					std::exit(EXIT_FAILURE);
+				if (result.ec == errc::invalid_argument) {
+					cerr << "Non-integer value in input_to_int\n";
+					exit(EXIT_FAILURE);
 				}
-				else if (result.ec == std::errc::result_out_of_range) {
-					std::cerr << "Integer type too small in input_to_int\n";
-					std::exit(EXIT_FAILURE);
+				else if (result.ec == errc::result_out_of_range) {
+					cerr << "Integer type too small in input_to_int\n";
+					exit(EXIT_FAILURE);
 				}
 				output[i].push_back(value);
 			}
@@ -32,31 +34,31 @@ namespace Utils
 
 	// Function to read input file "file_name" containing lines split by multiple delimiters
 	// and output a Cont1(vector) of Cont2(vector) of strings called "input"
-	template <template <typename...> typename Cont2 = std::vector, template <typename...> typename Cont1 = std::vector >
-	Cont1<Cont2<std::string>> ReadInput2D(std::string file_name, std::vector<std::string> delimiters) {
+	template <template <typename...> typename Cont2 = vector, template <typename...> typename Cont1 = vector >
+	Cont1<Cont2<string>> ReadInput2D(string file_name, vector<string> delimiters) {
 
 		if (delimiters.empty()) {
-			std::cout << "No delimiters provided. Use read_input instead of read_input_2D" << std::endl;
-			std::exit(EXIT_FAILURE);
+			cout << "No delimiters provided. Use read_input instead of read_input_2D" << endl;
+			exit(EXIT_FAILURE);
 		}
 
 		// output vector of strings
-		Cont1<Cont2<std::string>> input;
+		Cont1<Cont2<string>> input;
 
 		// read input into "line"
-		std::string line;
-		std::ifstream input_file(file_name);
+		string line;
+		ifstream input_file(file_name);
 
 		// check that file exists
 		if (!input_file.is_open()) {
-			std::cout << "Could not open file " << file_name << std::endl;
-			std::exit(EXIT_FAILURE);
+			cout << "Could not open file " << file_name << endl;
+			exit(EXIT_FAILURE);
 		}
 
 		size_t line_length;
 		size_t delims = delimiters.size();
-		std::string temp_val;
-		Cont2<std::string> temp_vector;
+		string temp_val;
+		Cont2<string> temp_vector;
 		while (getline(input_file, line)) {
 
 			line_length = line.size();
@@ -97,77 +99,81 @@ namespace Utils
 
 	// Function to read input file "file_name" containing values separated by
 	// "separator" and output a Cont(vector) of strings called "input"
-	template <template <typename...> typename Cont = std::vector>
-	Cont<std::string> ReadInput(std::string file_name, std::vector<std::string> delimiters = {}) {
+	template <template <typename...> typename Cont = vector>
+	Cont<string> ReadInput(string file_name, vector<string> delimiters = {}) {
 
 		// output vector of strings
-		Cont<std::string> input;
+		Cont<string> input;
 
 		// read input into "line"
-		std::string line;
-		std::ifstream input_file(file_name);
+		string line;
+		ifstream input_file(file_name);
 
 		// check that file exists
-		if (!(input_file.is_open())) {
-			std::cout << "Could not open file " << file_name << std::endl;
-			std::exit(EXIT_FAILURE);
+		if (!(input_file.is_open())) 
+		{
+			cout << "Could not open file " << file_name << endl;
+			exit(EXIT_FAILURE);
 		}
 
 		size_t line_length;
-		std::string temp_val;
+		string temp_val;
 		size_t delims = delimiters.size();
 
-		while (getline(input_file, line)) {
-
+		while (getline(input_file, line)) 
+		{
 			line_length = line.size();
-
-			if (delimiters.empty()) { input.push_back(line); }
-			else {
+			if (delimiters.empty()) 
+			{ 
+				input.push_back(line); 
+			}
+			else 
+			{
 				// loop through contents of line
-				for (size_t read_pos = 0; read_pos < line_length; read_pos++) {
-
-					for (size_t i = 0; i < delims; i++) {
-
+				for (size_t read_pos = 0; read_pos < line_length; read_pos++) 
+				{
+					for (size_t i = 0; i < delims; i++) 
+					{
 						// if next characters != any delimiter, add next char to temp_val
-						if (line.substr(read_pos, delimiters[i].size()) == delimiters[i]) {
+						if (line.substr(read_pos, delimiters[i].size()) == delimiters[i]) 
+						{
 							read_pos += delimiters[i].size() - 1;
 							// in case delimiters follow each other
 							if (!temp_val.empty()) { input.push_back(temp_val); }
 							temp_val.clear();
 							break;
 						}
-						else {
-							if (i == delimiters.size() - 1) {
+						else 
+						{
+							if (i == delimiters.size() - 1)
 								temp_val.push_back(line[read_pos]);
-							}
 						}
 					}
 				}
 				// push_back last value
-				if (!temp_val.empty()) { input.push_back(temp_val); }
+				if (!temp_val.empty())
+					input.push_back(temp_val);
 				temp_val.clear();
 			}
 		}
-
 		input_file.close();
-
 		return input;
 	}
 
 
 	template <typename T>
-	std::vector<T> SplitString(const std::string& str, const std::string& delimiters) 
+	vector<T> SplitString(const string& str, const string& delimiters) 
 	{
-		std::vector<T> tokens;
-		std::string token;
-		std::stringstream ss(str);
+		vector<T> tokens;
+		string token;
+		stringstream ss(str);
 
-		while (std::getline(ss, token, delimiters[0])) 
+		while (getline(ss, token, delimiters[0])) 
 		{
-			for (std::size_t i = 1; i < delimiters.length(); ++i) 
+			for (size_t i = 1; i < delimiters.length(); ++i) 
 			{
-				std::string::size_type pos = 0;
-				while ((pos = token.find(delimiters[i], pos)) != std::string::npos)
+				string::size_type pos = 0;
+				while ((pos = token.find(delimiters[i], pos)) != string::npos)
 					token.replace(pos, 1, " ");
 			}
 			tokens.push_back(token);
@@ -179,6 +185,34 @@ namespace Utils
 	template<typename T>
 	inline void PrintVector(const T& t)
 	{
-		std::copy(t.cbegin(), t.cend(), std::ostream_iterator<typename T::value_type>(std::cout, ", "));
+		copy(t.cbegin(), t.cend(), ostream_iterator<typename T::value_type>(cout, ", "));
+	}
+
+
+	inline vector<int> StringToVectorOfInts(const string& str)
+	{
+		vector<int> result;
+		for (char c : str) 
+		{
+			if (c >= '0' && c <= '9')
+				result.push_back(c - '0');
+		}
+		return result;
+	}
+
+	inline vector<vector<int>> StringToVectorOfInts(const vector<string>& strs) 
+	{
+		vector<vector<int>> result;
+		for (const auto& str : strs) 
+		{
+			vector<int> digits;
+			for (const auto& c : str) 
+			{
+				if (c >= '0' && c <= '9')
+					digits.push_back(c - '0');
+			}
+			result.push_back(digits);
+		}
+		return result;
 	}
 }
