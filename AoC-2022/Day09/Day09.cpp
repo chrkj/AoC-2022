4963d8959307f79d3d3900b91f2df5222a1b86fd
Christian Kjær <c-kj@mail.dk>
2022-12-08 19:48:13 +0100

    Added day09

---
+++ AoC-2022/Day09/Day09.cpp
@@ -0,0 +1,15 @@
+#include "Day09.h"
+
+#define INPUT "Day09/TEST.IN"
+
+void Day09::Part1()
+{
+	ScopedTimer timer;
+
+}
+
+void Day09::Part2()
+{
+	ScopedTimer timer;
+
+}
---
+++ AoC-2022/Day09/Day09.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "Core/AocDay.h"
+
+class Day09 : public AocDay
+{
+public:
+	void AocDay::Part1();
+	void AocDay::Part2();
+};
